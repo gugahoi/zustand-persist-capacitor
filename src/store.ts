44c257eca0df20f17ffbdc29667067242eedf4f2
Gustavo Hoirisch <gugahoi@users.noreply.github.com>
2022-07-13 10:44:28 +1000

    Add persist layer

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,20 @@
+import { Storage } from "@capacitor/storage";
 import create from "zustand";
+import {persist} from "zustand/middleware";
 
 type Store = {
   signedUp: boolean,
   setSignedUp: () => void,
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create(persist<Store>((set) => ({
   signedUp: false,
-  setSignedUp: () => set(s => ({signedUp: true}))
+  setSignedUp: () => set(() => ({signedUp: true}))
+}), {
+  name:'zustand-persist-capacitor',
+  getStorage: () => ({
+    getItem: (key) => Storage.get({ key }).then(({ value }) => value),
+    setItem: (key, value) => Storage.set({ key, value }),
+    removeItem: (key) => Storage.remove({ key }),
+  })
 }))
